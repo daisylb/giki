e16639091938e99977f6b0495557b64de7180f3d
Adam Brenecki <adam@brenecki.id.au>
2012-07-13 12:01:58 +0930

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+setup(name='giki',
+	version='0.1pre',
+	description='a Git-based wiki',
+	author='Adam Brenecki',
+	author_email='adam@brenecki.id.au',
+	url='',
+	packages=['.'.join(i[0].split(os.sep))
+		for i in os.walk('giki')
+		if '__init__.py' in i[2]],
+	install_requires=[
+	],
+	entry_points={
+    'console_scripts':
+        ['giki = giki.cli:main'],
+	},
+)
