5ed587e6e1917c07dd65f620e42253263192cf52
Adam Brenecki <adam@brenecki.id.au>
2012-08-22 20:30:59 +0930

    fix tests so they actually pass

--- test/test_web_framework.py
+++ test/test_web_framework.py
@@ -1,21 +1,21 @@
-from giki.web_framework import WebApp, bind, get, post
+from giki.web_framework import WebApp, bind, get, post, Response
 
-class TestApp (WebApp):
+class _TestApp (WebApp):
 	@bind(r'^/bind', ['GET', 'POST'])
-	def test_bind(self):
+	def test_bind(self, request):
 		pass
 	
 	@get(r'^/get')
-	def test_get(self):
-		pass
+	def test_get(self, request):
+		return Response(content='abc')
 	
 	@post(r'^/post')
-	def test_post(self):
+	def test_post(self, request):
 		pass
 
 def test_bind_decorator():
 	
-	test_app = TestApp()
+	test_app = _TestApp()
 	
 	assert test_app.test_bind._is_web_view
 	assert test_app.test_get._is_web_view
@@ -28,3 +28,14 @@ def test_bind_decorator():
 	assert test_app.test_bind.verbs == ['GET', 'POST']
 	assert test_app.test_get.verbs == ['GET']
 	assert test_app.test_post.verbs == ['POST']
+
+def test_routes():
+	test_app = _TestApp()
+	
+	environ = {
+		'REQUEST_METHOD': 'GET',
+		'PATH_INFO': '/get/banana'
+	}
+		
+	assert test_app.wsgi()(environ, lambda x, y: x) == ['abc']
+	
